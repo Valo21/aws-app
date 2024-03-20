2e153556d8025838f0698b3a11608e62024649bf
Valentín <61363314+Valo21@users.noreply.github.com>
2024-03-20 12:39:49 -0300

    feat(backend): enable cors for frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api/');
   app.use(cookieParser());
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    methods: ['GET', 'POST', 'PATCH'],
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
