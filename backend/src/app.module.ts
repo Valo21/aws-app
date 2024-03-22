7ff0d31d76e2e6d38285cbc6b020507132dd6807
Valentín <61363314+Valo21@users.noreply.github.com>
2024-03-22 02:50:09 -0300

    fix: update ssl

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -27,7 +27,9 @@ import { ProfilePhotosModule } from './v1/profile-photos/profile-photos.module';
       database: process.env.DATABASE_NAME,
       username: process.env.DATABASE_USERNAME,
       password: process.env.DATABASE_PASSWORD,
-      ssl: false,
+      ssl: {
+        rejectUnauthorized: false
+      },
       synchronize: true,
       logging: 'all',
       autoLoadEntities: true,
