650467ef5aecce77e0b096d045021bf9d3118e4d
Valentín <61363314+Valo21@users.noreply.github.com>
2024-03-24 21:00:29 -0300

    feat(backend): add validation for dtos

--- backend/src/v1/users/dto/create-user.dto.ts
+++ backend/src/v1/users/dto/create-user.dto.ts
@@ -1,6 +1,11 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty()
   username: string;
+  @IsNotEmpty()
   fullName: string;
+  @IsNotEmpty()
   password: string;
   image: string;
 }
